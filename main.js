6380e6fbf1f60b6895739fed61a8502d8c561a29
Md. Sakib Al Hasan <sakibalhasan17442@gmail.com>
2022-02-16 18:53:51 +0600

    Included Dynamic contents
    Thanks @chocosinensis

---
+++ main.js
@@ -0,0 +1,13 @@
+(async () => {
+  const data = await fetch("/data.json").then((r) => r.json());
+
+  document.querySelector(".cont").innerHTML = data
+    .map(
+      ({ href, text }) => `
+
+    <a class="button" href="${href}">${text}</a></br>
+
+  `
+    )
+    .join("");
+})();
