00a2c3b035516c872340e9eb35d97978e4e4ec7f
Md. Sakib Al Hasan <sakibalhasan17442@gmail.com>
2022-02-17 18:32:42 +0600

    Yoo

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 (async () => {
-
   const data = await fetch("data.json").then((r) => r.json());
 
-  document.querySelector(".cont").innerHTML = data.map(({ href, text }) => `
-
-    <a class="button" href="${href}">${text}</a><br/>
-
-  `).join("");
-
+  document.querySelector(".cont").innerHTML = data
+    .map(
+      ({ href, text }) => `
+    <a class="button" href="${href}">${text}</a></br>
+  `
+    )
+    .join("");
 })();
